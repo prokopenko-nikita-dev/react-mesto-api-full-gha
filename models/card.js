4df62e2c38caeed24e5d343a6dcc1358b29b42a6
girolle <girolle@github.com>
2021-10-20 10:56:31 +0200

    add canonical

---
+++ models/card.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const cardsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "name" - 2'],
+    maxlength: [30, 'Максимальная длина поля "name" - 30'],
+  },
+  link: {
+    type: String,
+    required: [true, 'Поле "link" должно быть заполнено'],
+    // Валидицию ссылок студенты будут делать на следующем спринте
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('card', cardsSchema);
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "name" - 2'],
+    maxlength: [30, 'Максимальная длина поля "name" - 30'],
+  },
+  about: {
+    type: String,
+    required: [true, 'Поле "about" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "about" - 2'],
+    maxlength: [30, 'Максимальная длина поля "about" - 30'],
+  },
+  avatar: {
+    type: String,
+    required: [true, 'Поле "avatar" должно быть заполнено'],
+    // Валидицию ссылок студенты будут делать на следующем спринте
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('user', userSchema);
